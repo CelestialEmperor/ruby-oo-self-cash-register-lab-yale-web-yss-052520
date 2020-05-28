0422eaadd34f556b68633c72c45cf20e965c77e9
Jack Ma <mapeiyao@gmail.com>
2020-05-28 13:07:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,6 +22,7 @@ class CashRegister
       puts "After the discount, the total comes $#{self.total}."
     else
       puts "There is no discount to apply."
+    end
   end
   
   def void_last_transaction
