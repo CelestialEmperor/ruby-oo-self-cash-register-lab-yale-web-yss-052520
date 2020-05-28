19bc406e58ca210de049c1f8f2ca3be9ebaa6053
Jack Ma <mapeiyao@gmail.com>
2020-05-28 13:06:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   
   def apply_discount
     if discount !=0
-      self.total = total * ((100.0 - discount.to.f)/100)
+      self.total = total * ((100.0 - discount.to.f)/100).to.i
   end
   
   def void_last_transaction
